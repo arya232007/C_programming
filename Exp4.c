f28acca61968701e9f76744e418482f906cb5065
arya232007 <aryavaze232007@eng.rizvi.edu.in>
2025-10-19 11:12:09 +0530

    Create Exp4.c

---
+++ Exp4.c
@@ -0,0 +1,36 @@
+name: arya vaze
+UIN: 251C013
+branch: civil
+div: B roll no: 10
+
+********************************************
+
+#include <stdio.h>
+
+int isPrime(int num) {
+    if (num <= 1) {
+        return 0;
+    }
+    for (int i = 2; i * i <= num; i++) {
+        if (num % i == 0) {
+            return 0;
+        }
+    }
+    return 1;
+}
+
+int main() {
+    int num1, num2;
+
+    printf("Enter the first number: ");
+    scanf("%d", &num1);
+
+    printf("Enter the second number: ");
+    scanf("%d", &num2);
+
+    if (num1 > num2) {
+        int temp = num1;
+        num1 = num2;
+        num2 = temp;
+    }
+
