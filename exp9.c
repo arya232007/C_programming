de00033dbc28c7476fe1dff3e84a856e925dcce1
arya232007 <aryavaze232007@eng.rizvi.edu.in>
2025-11-21 21:38:44 +0530

    Create exp9.c

---
+++ exp9.c
@@ -0,0 +1,19 @@
+name: arya vaze
+UIN:251C013
+roll no:251C010
+branch:civil
+***************************************************
+#include <stdio.h>
+
+int main() {
+    int a, b, sum;
+    int *pntr1, *pntr2;
+
+    printf("Enter first number: ");
+    scanf("%d", &a);
+
+    printf("Enter second number: ");
+    scanf("%d", &b);
+
+    pntr1 = &a;
+    pn
